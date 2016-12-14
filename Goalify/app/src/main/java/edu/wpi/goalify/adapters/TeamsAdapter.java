be0421d0f94b5e6bb2d44d36950f8619dccabfa7
Jules Voltaire <javoltaire@wpi.edu>
2016-12-13 19:13:57 -0500

    noti

--- Goalify/app/src/main/java/edu/wpi/goalify/adapters/TeamsAdapter.java
+++ Goalify/app/src/main/java/edu/wpi/goalify/adapters/TeamsAdapter.java
@@ -10,9 +10,17 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import edu.wpi.goalify.R;
+import edu.wpi.goalify.models.FollowedTeam;
 import edu.wpi.goalify.models.Team;
 import edu.wpi.goalify.models.TeamLocation;
 import edu.wpi.goalify.sqlite.DBHelper;
@@ -25,10 +33,6 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
 
     //list of followed teams
     private static ArrayList<Team> followedTeamArrayList;
-
-    private Team team;
-
-    private ToggleButton followTeamBtn;
 
     private DBHelper dbHelper;
 
@@ -49,20 +53,43 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
 
         Cursor cursor = dbHelper.getAllTeams();
 
-        if (cursor.moveToFirst()){
-            while(cursor.moveToNext()){
-                int teamID = cursor.getInt(0);
-                String teamName = cursor.getString(1);
-                double lat = cursor.getDouble(2);
-                double lon = cursor.getDouble(3);
+        cursor.moveToFirst();
 
-                Team t = new Team(teamID, teamName, new TeamLocation(lat, lon));
-                followedTeamArrayList.add(t);
-                System.out.println(followedTeamArrayList);
-                // do what ever you want here
-            }
+        while(!cursor.isAfterLast()){
+            int teamID = cursor.getInt(0);
+            String teamName = cursor.getString(1);
+            double lat = cursor.getDouble(2);
+            double lon = cursor.getDouble(3);
+
+            Team t = new Team(teamID, teamName, new TeamLocation(lat, lon));
+            followedTeamArrayList.add(t);
+
+            cursor.moveToNext();
         }
         cursor.close();
+
+//        printList();
+//        deleteList();
+        printList();
+
+
+    }
+
+    private void printList() {
+        if (followedTeamArrayList != null){
+            for (Team team: followedTeamArrayList){
+                System.out.println("printling listtttttttttttt" + team.getTeamName());
+            }
+        }
+    }
+
+    private void deleteList() {
+        if (followedTeamArrayList != null){
+            for (Team team: followedTeamArrayList){
+                System.out.println("deleting listtttttttttttt" + team.getTeamName());
+                dbHelper.deleteTeam(team.getTeamId());
+            }
+        }
     }
     //endregion
 
@@ -74,7 +101,8 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
 
 
         // Grab the data item for this position, make sure that it is not null
-        team = getItem(position);
+        final Team team = getItem(position);
+
         if(team == null)
             return super.getView(position, convertView, parent);
 
@@ -90,9 +118,9 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
         tvTeamName.setText(team.getTeamName());
 
         //toggle button
-        followTeamBtn = (ToggleButton) convertView.findViewById(R.id.item_followTeam_toggleButton);
+        final ToggleButton followTeamBtn = (ToggleButton) convertView.findViewById(R.id.item_followTeam_toggleButton);
 
-        setupFollowButton();
+        setupFollowButton(followTeamBtn, team);
 
         followTeamBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,28 +130,53 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
 
                 //check if team is a favorite
                 if (followedTeamArrayList != null){
-                    isFav = doesContain(followedTeamArrayList, team);
-                    index = followedTeamArrayList.indexOf(team);
+                    isFav = doesContain(team);
                 }
 
                 if (!isFav){  //team is not a favorite
 
-                    System.out.println("Team not a favorite but should be now");
+                    //System.out.println("Team not a favorite but should be now");
 
                     //make team a favorite
                     followedTeamArrayList.add(team);
+
+
+                    FirebaseMessaging.getInstance().subscribeToTopic(team.getTeamName());
+                    System.out.println("Subscribed to " + team.getTeamName());
+
+                    DatabaseReference followedTeamsRef = FirebaseDatabase.getInstance().getReference().child("followedTeams");
+                    Map<String, FollowedTeam> teamsMap = new HashMap<>();
+                    if(followedTeamArrayList!=null)
+                    {
+                        for (int i = 0; i < followedTeamArrayList.size(); i++) {
+                            Team team = followedTeamArrayList.get(i);
+                            teamsMap.put(String.valueOf(team.getTeamId()), new FollowedTeam(team.getTeamId(), team.getTeamName()));
+                        }
+                    }
+
+                    followedTeamsRef.setValue(teamsMap);
 
                     followTeamBtn.setChecked(true);
 
                 } else { //team is a favorite
 
-                    System.out.println("Team a favorite but shouldn't be now");
+                    //System.out.println("Team a favorite but shouldn't be now");
 
                     //un-favorite team
-                    if (index != -1) {
-                        dbHelper.deleteTeam(followedTeamArrayList.get(index).getTeamId());
-                        followedTeamArrayList.remove(index);
-                    }
+
+                    dbHelper.deleteTeam(team.getTeamId());
+
+                    removeTeam(team.getTeamId());
+
+
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic(team.getTeamName());
+                    System.out.println("Unsubscribed from " + team.getTeamName());
+
+                    FirebaseDatabase.getInstance().
+                            getReference().child("followedTeams")
+                            .child(String.valueOf(team.getTeamId()))
+                            .removeValue();
+
 
                     followTeamBtn.setChecked(false);
 
@@ -133,6 +186,7 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
                 //save the array list
                 if (followedTeamArrayList != null){
                     for (Team t: followedTeamArrayList){
+                        System.out.println("**************adding a team*********************");
                         dbHelper.insertTeam(t.getTeamId(), t.getTeamName(), t.getTeamLocation().getTeamLatitude(), t.getTeamLocation().getTeamLongitude());
                     }
                 }
@@ -145,29 +199,48 @@ public class TeamsAdapter extends ArrayAdapter<Team> {
         return convertView;
     }
 
+    private void removeTeam(int id) {
+        Iterator<Team> it = followedTeamArrayList.iterator();
+        while (it.hasNext()) {
+            if (it.next().getTeamId() == id) {
+                it.remove();
 
-    private void setupFollowButton() {
+
+                System.out.println(" ...............deleted...........");
+            }
+        }
+
+
+    }
+
+
+    private void setupFollowButton(ToggleButton followTeamBtn,  Team team) {
 
         boolean isFav = false;
 
         //check if the team is a favorite
         if (followedTeamArrayList != null){
-            isFav = doesContain(followedTeamArrayList, team);
+
+            isFav = doesContain(team);
         }
 
         if (!isFav){  //airlines is not a favorite
+
+            //System.out.println("**************is not a favorite team*********************");
 
             //un-fill favorite image
             followTeamBtn.setChecked(false);
         } else { //team is a favorite
 
+
+            System.out.println("**************is a favorite team*********************");
             //fill the favorite image
             followTeamBtn.setChecked(true);
 
         }
     }
 
-    private boolean doesContain(ArrayList<Team> followedTeamArrayList, Team team) {
+    private boolean doesContain(Team team) {
         for (Team t: followedTeamArrayList){
             if (t.getTeamName().equals(team.getTeamName())){
                 return true;
